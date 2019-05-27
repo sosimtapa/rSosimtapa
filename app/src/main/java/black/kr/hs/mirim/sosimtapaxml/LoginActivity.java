68c8f445baeeefc925812fe3d010e352e4cbb625
Hyoeun Jung <hyodaeun@naver.com>
2019-05-27 19:07:15 +0900

    signup and signin

--- app/src/main/java/black/kr/hs/mirim/sosimtapaxml/LoginActivity.java
+++ app/src/main/java/black/kr/hs/mirim/sosimtapaxml/LoginActivity.java
@@ -2,11 +2,26 @@ package black.kr.hs.mirim.sosimtapaxml;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 /**
  * Created by 정효은 on 2019-04-06.
@@ -14,11 +29,15 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText id;
+    private EditText pw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        //회원가입으로 이동하는 버튼
         TextView signupButton = (TextView) findViewById(R.id.signupButton);
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +52,42 @@ public class LoginActivity extends AppCompatActivity {
         first_btn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), testStart.class);
-                startActivity(intent);
+                loginCheck();
             }
+
         });
+
+    }
+
+    private void loginCheck() {
+
+        id = findViewById(R.id.idText);
+        pw = findViewById(R.id.passwordText);
+
+        FirebaseFirestore userDB = FirebaseFirestore.getInstance();
+
+        userDB.collection("signUp")
+                .whereEqualTo("userID", id.getText().toString()).whereEqualTo("userPW", pw.getText().toString())
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        //테스트화면에서 출력하도록 바꾸기
+                        Toast.makeText(LoginActivity.this, "로그인 성공" + document.getString("userID") + "님 환영합니다.", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(getApplicationContext(), testStart.class);
+                        startActivity(intent);
+
+                    }
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "로그인 실패" + task.getException(), Toast.LENGTH_SHORT).show(); // 왜 안뜨는 지 모름
+                }
+            }
+
+        });
+
+
     }
 }
--- app/src/main/java/black/kr/hs/mirim/sosimtapaxml/SignupActivity.java
+++ app/src/main/java/black/kr/hs/mirim/sosimtapaxml/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,9 +14,15 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,12 +44,25 @@ public class SignupActivity extends AppCompatActivity {
     private RadioButton genderRB;
     private String hobby;
     private String signUpId;
+    private int count;
+    private String totCnt;
+
+    public String getTotCnt() {
+
+        return totCnt;
+    }
+
+    public void setTotCnt(String totCnt) {
+
+        this.totCnt = totCnt;
+        Toast.makeText(SignupActivity.this,"Count : "+ totCnt,Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
-
+//
         userDB = FirebaseFirestore.getInstance();
 
         id = findViewById(R.id.idText);
@@ -64,20 +84,41 @@ public class SignupActivity extends AppCompatActivity {
                 signUpId = userDB.collection("signUp").document().getId();
                 genderRB = (RadioButton)findViewById(genderRG.getCheckedRadioButtonId());
 
+//                userDB.collection("signUp")
+//                        .get()
+//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            @Override
+//
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    count = 0;
+//                                    for (DocumentSnapshot document : task.getResult()) {
+//                                        count++;
+//                                    }
+//                                    String tot = String.valueOf(count);
+//                                    setTotCnt(tot);
+//
+//                                } else {
+//                                    Toast.makeText(SignupActivity.this,"Error getting documents: "+ task.getException(),Toast.LENGTH_SHORT).show();
+//                                }
+//                            }
+//                        });
+
                 Map<String,Object> post = new HashMap<>();
-                post.put("signUpID",signUpId);
+
+                post.put("signUpID",getTotCnt());
                 post.put("userID",id.getText().toString());
                 post.put("userPW",pw.getText().toString());
                 post.put("userEmail",email.getText().toString());
                 post.put("userGender", genderRB.getText().toString());
                 post.put("userHobby",hobby);
 
-                //
-                userDB.collection("signUp").document(signUpId)
+                userDB.collection("signUp").document()
                         .set(post)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
+
                                 Toast.makeText(SignupActivity.this,"회원가입 성공!",Toast.LENGTH_SHORT).show();
                                 finish();
                             }
@@ -88,10 +129,12 @@ public class SignupActivity extends AppCompatActivity {
                                 Toast.makeText(SignupActivity.this,"회원가입 실패",Toast.LENGTH_SHORT).show();
                             }
                         });
-                //
+
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
+
         });
+
     }
 }
